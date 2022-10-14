4c1da08a249b9b27d88663ee15a15349b5689740
Patrick Huynh <pldhuynh@gmail.com>
2022-10-14 11:31:54 +1000

    temporary fix for outlines modal box on mobile

--- src/components/outlines/logic/LogicReasons.js
+++ src/components/outlines/logic/LogicReasons.js
@@ -6,22 +6,25 @@ const LamRimChenMo = () => {
   const [modalShow, setModalShow] = useState(true);
   return (
     <Container fluid>
-      <Modal show={modalShow} onHide={() => setModalShow(false)} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">Outline Navigation Tip</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Scroll through the image using the middle mouse when on desktop, or dragging the image when on a phone</p>
-        </Modal.Body>{" "}
-        <Modal.Footer>
-          <Button onClick={() => setModalShow(false)}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-      <Row>
-        <Col>
-          <img src="https://raw.githubusercontent.com/PatrickHuynh/dharma-outlines/main/logic/logic%20-%20reasons.svg" />
-        </Col>
-      </Row>
+      {modalShow ? (
+        <Modal show={modalShow} onHide={() => setModalShow(false)} aria-labelledby="contained-modal-title-vcenter" centered>
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">Outline Navigation Tip</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Scroll through the image using the middle mouse when on desktop, or dragging the image when on a phone</p>
+          </Modal.Body>{" "}
+          <Modal.Footer>
+            <Button onClick={() => setModalShow(false)}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      ) : (
+        <Row>
+          <Col>
+            <img src="https://raw.githubusercontent.com/PatrickHuynh/dharma-outlines/main/logic/logic%20-%20reasons.svg" />
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 };
--- src/components/outlines/lrcm/LamRimChenMo.js
+++ src/components/outlines/lrcm/LamRimChenMo.js
@@ -6,22 +6,25 @@ const LamRimChenMo = () => {
   const [modalShow, setModalShow] = useState(true);
   return (
     <Container fluid>
-      <Modal show={modalShow} onHide={() => setModalShow(false)} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">Outline Navigation Tip</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Scroll through the image using the middle mouse when on desktop, or dragging the image when on a phone</p>
-        </Modal.Body>{" "}
-        <Modal.Footer>
-          <Button onClick={() => setModalShow(false)}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-      <Row>
-        <Col>
-          <img src="https://raw.githubusercontent.com/PatrickHuynh/dharma-outlines/main/lam%20rim%20chen%20mo/lam%20rim%20chen%20mo%20-%20outline.svg" />
-        </Col>
-      </Row>
+      {modalShow ? (
+        <Modal show={modalShow} onHide={() => setModalShow(false)} aria-labelledby="contained-modal-title-vcenter" centered>
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">Outline Navigation Tip</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Scroll through the image using the middle mouse when on desktop, or dragging the image when on a phone</p>
+          </Modal.Body>{" "}
+          <Modal.Footer>
+            <Button onClick={() => setModalShow(false)}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      ) : (
+        <Row>
+          <Col>
+            <img src="https://raw.githubusercontent.com/PatrickHuynh/dharma-outlines/main/lam%20rim%20chen%20mo/lam%20rim%20chen%20mo%20-%20outline.svg" />
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 };
