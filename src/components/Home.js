c5d0a23591f83bbfeb1826382e953f5cc7bbe8ca
Patrick Huynh <pldhuynh@gmail.com>
2022-10-16 17:07:31 +1000

    changed home screen introduction and layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,31 +12,35 @@ const Home = () => {
       <Row>
         <Col className="p-3 text-center">
           <h1 className="fw-bold text-white">Dharmasaurus welcomes you!</h1>
-          <p className="text-white">Dharmasaurus collects notes on the dharma. Dharmasaurus hopes these references are of benefit but also cautions that they are at best personal notes of the Dharmasaur.</p>
+          <p className="text-white">Dharmasaurus collects notes on the dharma. These references may be of benefit, but take caution that they are at best just notes of the Dharmasaur.</p>
         </Col>
       </Row>
-      <Row className="p-4">
-        <Col className="d-flex justify-content-center">
-          <Card className="mx-3 bg-secondary" style={{ width: "18rem" }}>
-            <Card.Body className="text-center text-white">
-              <Card.Title>Definitions</Card.Title>
-              <Card.Text>Search all your favourite definitions</Card.Text>
-              <LinkContainer to="definitions" style={{ textDecoration: "none" }}>
-                <Button variant="warning">{"Go »"}</Button>
-              </LinkContainer>
-            </Card.Body>
-          </Card>
-          <Card className="mx-3 bg-secondary" style={{ width: "18rem" }}>
-            <Card.Body className="text-center text-white">
-              <Card.Title>Maps</Card.Title>
-              <Card.Text>Maps for various subjects</Card.Text>
-              <LinkContainer to="maps" style={{ textDecoration: "none" }}>
-                <Button variant="warning">{"Go »"}</Button>
-              </LinkContainer>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      <Container>
+        <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 justify-content-center">
+          <Col className="justify-content-center">
+            <Card className="mx-3 bg-secondary">
+              <Card.Body className="text-center text-white">
+                <Card.Title>Definitions</Card.Title>
+                <Card.Text>Search all your favourite definitions</Card.Text>
+                <LinkContainer to="definitions" style={{ textDecoration: "none" }}>
+                  <Button variant="warning">{"Go »"}</Button>
+                </LinkContainer>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className="justify-content-center">
+            <Card className="mx-3 bg-secondary">
+              <Card.Body className="text-center text-white">
+                <Card.Title>Maps</Card.Title>
+                <Card.Text>Maps for various subjects</Card.Text>
+                <LinkContainer to="maps" style={{ textDecoration: "none" }}>
+                  <Button variant="warning">{"Go »"}</Button>
+                </LinkContainer>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 };
