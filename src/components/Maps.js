2670c92dc1bd7f312ceb999d4205f894a08042f9
Patrick Huynh <pldhuynh@gmail.com>
2022-10-17 13:23:53 +1000

    refactored maps menu

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,29 +1,36 @@
-import { Container, Row, Col, Button, Card, Spinner, Stack, Collapse, ListGroup, Offcanvas } from "react-bootstrap";
+import { Container, Row, Col, Button, Card, Spinner, Stack, Collapse, ListGroup, Offcanvas, Dropdown } from "react-bootstrap";
 import { useState, useEffect, useRef } from "react";
-import { LinkContainer } from "react-router-bootstrap";
 
 const Maps = () => {
   const [showMenu, setShowMenu] = useState(true);
   const [mapLoading, setMapLoading] = useState(true);
-  const [currentMap, setCurrentMap] = useState({ text: "Mind and Mental Factors", url: "mmf.svg", parent: false, key: "mmf" });
+  const [currentMap, setCurrentMap] = useState({ text: "Mind and Mental Factors", url: "mmf.svg", parent: "mmf", key: "mmf" });
+  const [mapNavStructure, setMapNavStructure] = useState({ headers: [], maps: [] });
 
   useEffect(() => {
-    fetchMap();
+    initialiseMapNavStructure();
+    setMapLoading(false);
   }, []);
 
   const repositoryUrl = "https://raw.githubusercontent.com/PatrickHuynh/dharmasaurus-data/main/maps/";
 
-  const mapPaths = {
-    mmf: { text: "Mind and Mental Factors", url: "mmf.svg", parent: false, key: "mmf" },
-    mmf_minds: { text: "Minds", url: "mmf_minds.svg", parent: "mmf", key: "mmf_minds" },
-    mmf_5omnipresent: { text: "Five Omnipresent", url: "mmf_5omnipresent.svg", parent: "mmf", key: "mmf_5omnipresent" },
-    mmf_5objasc: { text: "Five Object Ascertaining", url: "mmf_5objasc.svg", parent: "mmf", key: "mmf_5objasc" },
-    mmf_11virtuous: { text: "Eleven Virtuous", url: "mmf_11virtuous.svg", parent: "mmf", key: "mmf_11virtuous" },
-    mmf_6rootaff: { text: "Six Root Afflictions", url: "mmf_6rootaff.svg", parent: "mmf", key: "mmf_6rootaff" },
-    mmf_20secaff: { text: "Twenty Secondary Afflictions", url: "mmf_20secaff.svg", parent: "mmf", key: "mmf_20secaff" },
-    mmf_4changeable: { text: "Four Changeable", url: "mmf_4changeable.svg", parent: "mmf", key: "mmf_4changeable" },
-    lrcm: { text: "Lam Rim Chen Mo", url: "lrcm.svg", parent: false, key: "lrcm" },
-    reasons: { text: "Reasons and Logic", url: "reasons.svg", parent: false, key: "reasons" },
+  const initialiseMapNavStructure = async () => {
+    const res = await fetch(repositoryUrl + "mapNavStructure.json");
+    let data = await res.json();
+    let headers = Object.keys(data).filter((v) => !v.includes("_"));
+    let mapObjects = Object.entries(data).map(([k, v]) => {
+      return {
+        key: k,
+        parent: k.split("_")[0],
+        text: v,
+        url: k + ".svg",
+      };
+    });
+    mapObjects = [...mapObjects].reduce((arr, curr) => ((arr[curr.key] = curr), arr), {});
+    setMapNavStructure({
+      headers: headers,
+      maps: mapObjects,
+    });
   };
 
   const fetchMap = async () => {
@@ -35,7 +42,7 @@ const Maps = () => {
     setShowMenu(false);
     await fetchMap();
     setMapLoading(false);
-    setCurrentMap({ ...mapPaths[key], key: key });
+    setCurrentMap({ ...mapNavStructure.maps[key] });
   };
 
   const handleCloseMenu = () => setShowMenu(false);
@@ -52,21 +59,24 @@ const Maps = () => {
   const crumbsList = () => {
     let crumbs = [];
     let el = currentMap;
-    do {
+    while (true) {
       const frag = (map) => {
         return (
           <>
-            {map.parent && " | "}
+            {map.key !== map.parent && " | "}
             <div key={map.key} className={`d-inline btn btn-sm ${map.key === currentMap.key ? "btn-dark" : "btn-outline-dark"}`} onClick={() => handleSelectMap(map.key)}>
               {map.text}
             </div>
           </>
         );
       };
-
       crumbs = [frag(el), ...crumbs];
-      el = mapPaths[el.parent];
-    } while (el);
+      if (mapNavStructure.maps[el.parent] == null) break;
+      else {
+        if (el.key == el.parent) break;
+        else el = mapNavStructure.maps[el.parent];
+      }
+    }
     return (
       <>
         {crumbs.map((map) => {
@@ -83,22 +93,27 @@ const Maps = () => {
           <Offcanvas.Title>Select Map</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <ListGroup>
-            {Object.entries(mapPaths).map(([key, value]) => {
-              let isActive = key === currentMap.key;
-              return (
-                <ListGroup.Item id={key} action onClick={() => handleSelectMap(key)} active={isActive}>
-                  {value.parent ? (
-                    <>{"> " + value.text}</>
-                  ) : (
-                    <b>
-                      <u>{value.text}</u>
-                    </b>
-                  )}
-                </ListGroup.Item>
-              );
-            })}
-          </ListGroup>
+          {mapNavStructure["headers"].map((headerKey) => {
+            return (
+              <Dropdown className="list-group" autoClose="outside">
+                <Dropdown.Toggle variant="dark" id="dropdown-autoclose-outside" className="my-2 p-2">
+                  {mapNavStructure.maps[headerKey].text}
+                </Dropdown.Toggle>
+                <Dropdown.Menu variant="secondary" className="w-100">
+                  {Object.entries(mapNavStructure.maps).map(([key, value]) => {
+                    let isActive = key === currentMap.key;
+                    return value.parent === headerKey ? (
+                      <Dropdown.Item id={key} onClick={() => handleSelectMap(key)} active={isActive}>
+                        {value.text}
+                      </Dropdown.Item>
+                    ) : (
+                      <></>
+                    );
+                  })}
+                </Dropdown.Menu>
+              </Dropdown>
+            );
+          })}
         </Offcanvas.Body>
       </Offcanvas>
 
