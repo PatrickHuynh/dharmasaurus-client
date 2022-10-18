e88aa6f8beb79eab2561e94f602926f03198e495
Patrick Huynh <pldhuynh@gmail.com>
2022-10-18 15:19:50 +1000

    added process env test

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 const Login = () => {
-  return <div className="m-3">Login under development</div>;
+  return <div className="m-3">{process.env.REACT_APP_TEST_MESSAGE}</div>;
 };
 
 export default Login;
